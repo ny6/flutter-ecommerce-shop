4f5baa517595d4770978cd5a36b654c24c94e7b4
Yeshu Kochher <yeshu@outlook.in>
2020-01-13 22:21:59 +0530

    one where confirm dialog added before deleting item in cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -18,6 +18,29 @@ class CartItemWidget extends StatelessWidget {
           Provider.of<Cart>(context, listen: false).removeItem(productId);
         }
       },
+      confirmDismiss: (direction) {
+        if (direction == DismissDirection.endToStart) {
+          return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+              title: Text('Are you sure?'),
+              content: Text('Do you want to remove the item from the cart?'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('No'),
+                  onPressed: () => Navigator.of(ctx).pop(false),
+                ),
+                FlatButton(
+                  child: Text('Yes'),
+                  onPressed: () => Navigator.of(ctx).pop(true),
+                ),
+              ],
+            ),
+          );
+        }
+
+        return Future.value(false);
+      },
       background: Container(
         color: Theme.of(context).errorColor,
         child: const Icon(Icons.delete, color: Colors.white, size: 40),
